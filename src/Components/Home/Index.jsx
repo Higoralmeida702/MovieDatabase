4a794c5d459b2371f3f800683d524049c962ac6c
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-07-31 18:08:14 -0300

    Adicionado conteúdo na pagina Home e sua estilização
    - Implementa a navegação para a página de filmes ao clicar no botão "Descubra +" usando `useNavigate`.
    - Adiciona uma seção de detalhes do site com informações sobre tendências, lançamentos e busca de filmes e séries.
    - Inclui imagens ilustrativas com descrições para cada seção de detalhes.

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -1,6 +1,45 @@
+import './Home.css'
+import { useNavigate } from 'react-router-dom';
+
+
+ 
 const Home = () => {
+     const navigate = useNavigate();
+     const handleNavigate = () => {
+        navigate('/movies');
+      };
+
     return (
-        <h1>Olá home</h1>
+        <div>
+
+        <div className='apresentacao'>
+            <h1 className='titulo'> Filmes,Series, Esportes e muito mais</h1>
+            <p className='subTitulo'>Explore o mundo do cinema e da TV com nosso poderoso motor de busca e descoberta. Aqui, você encontrará uma vasta coleção de informações detalhadas sobre filmes, séries de TV, atores e muito mais. Nosso site oferece</p>
+            <button onClick={handleNavigate} className='buttonDescubra'>Descubra +</button>
+        </div>
+            <section className='siteDetails'>
+                <div className='siteDetailsContent'>
+                    <img src="/Images/happykid.jpeg" alt="" className='imageHome'/>
+                    <h1>Fique dentro das tendências</h1>
+                    <p>Descubra os filmes e séries que estão em alta no momento, com listas atualizadas semanalmente.</p>
+                </div>
+                <div className='siteDetailsContent'>
+                    <img src="/Images/ironman.avif" alt="" className='imageHome'/>
+                    <h1>Saiba dos últimos lançamentos:</h1>
+                    <p>Fique por dentro dos últimos lançamentos nos cinemas e nas plataformas de streaming.</p>
+                </div>
+                <div className='siteDetailsContent'>
+                    <img src="/Images/moviecast.png" alt="" className='imageHome'/>
+                    <h1>Busca de Filmes e Séries</h1>
+                    <p>Encontre informações completas sobre seus filmes e séries de TV favoritos, incluindo sinopses, elenco, trailers e imagens</p>
+                </div>
+                <div className='siteDetailsContent'>
+                    <img src="/Images/lupa.png" alt="" className='imageHome'/>
+                    <h1>Busca de Filmes e Séries</h1>
+                    <p>Encontre informações completas sobre seus filmes e séries de TV favoritos, incluindo sinopses, elenco, trailers e imagens</p>
+                </div>
+            </section>
+        </div>
     )
 }
 
