d54072ad18579b8df17a4d1720575b6f75b0d2db
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-08-02 23:45:41 -0300

    Implementada busca de filmes populares e de gênero romance usando API do TMDb
    - Adicionada função para buscar os 10 filmes mais populares e os 10 filmes de romance mais populares.
    - Utilizado useEffect para chamar a API do TMDb e obter os dados dos filmes.
    - Adicionadas referências useRef para listas de filmes populares e de romance.
    - Implementadas funções scrollLeft e scrollRight para rolagem horizontal das listas de filmes.

--- src/Components/Movies/Index.jsx
+++ src/Components/Movies/Index.jsx
@@ -1,7 +1,95 @@
-const Movies = () => {
-    return (
-        <h1>Olá Movies</h1>
-    )
-}
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import './Movies.css';
 
-export default Movies
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [romanceMovies, setRomanceMovies] = useState([]);
+  const apiKey = '71f48d983a1d7c2c8e64239136feee27';
+  const moviesListRef = useRef(null);
+  const romanceMoviesListRef = useRef(null);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`);
+        const movieList = response.data.results.slice(0, 10);
+        setMovies(movieList);
+      } catch (error) {
+        console.error('Erro ao obter os filmes populares:', error);
+      }
+    };
+
+    fetchPopularMovies();
+  }, []);
+
+  useEffect(() => {
+    const fetchRomanceMovies = async () => {
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=10749&language=pt-BR`);
+        const romanceList = response.data.results.slice(0, 10);
+        setRomanceMovies(romanceList);
+      } catch (error) {
+        console.error('Erro ao obter os filmes de romance:', error);
+      }
+    };
+
+    fetchRomanceMovies();
+  }, []);
+
+  const scrollLeft = (ref) => {
+    if (ref.current) {
+      ref.current.scrollBy({ left: -250, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = (ref) => {
+    if (ref.current) {
+      ref.current.scrollBy({ left: 250, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className='tituloPopularesFilmes'>
+      <h1>Os 10 filmes mais populares no momento</h1>
+      <div className="movies-container">
+        <button className="scroll-button left" onClick={() => scrollLeft(moviesListRef)}>←</button>
+        <div className="movies-list" ref={moviesListRef}>
+          {movies.map(movie => (
+            <div key={movie.id} className="movie-item">
+              <img 
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                alt={movie.title} 
+                className="posterFilme"
+              />
+              <h3>{movie.title}</h3>
+            </div>
+          ))}
+        </div>
+        <button className="scroll-button right" onClick={() => scrollRight(moviesListRef)}>→</button>
+      </div>
+
+      <section>
+        <h1>Os 10 filmes de Romance mais populares no momento</h1>
+        <div className="movies-container">
+          <button className="scroll-button left" onClick={() => scrollLeft(romanceMoviesListRef)}>←</button>
+          <div className="movies-list" ref={romanceMoviesListRef}>
+            {romanceMovies.map(movie => (
+              <div key={movie.id} className="movie-item">
+                <img 
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                  alt={movie.title} 
+                  className="posterFilme"
+                />
+                <h3>{movie.title}</h3>
+              </div>
+            ))}
+          </div>
+          <button className="scroll-button right" onClick={() => scrollRight(romanceMoviesListRef)}>→</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Movies;
