f33e3dcff3c48c5e299a6fa9cd40e8e16fdebeee
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-08-07 15:37:34 -0300

    Remoção do redirecionamento de link Trendings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import './Styles.css'
 import Home from './Components/Home/Index'
-import Movie from './Components/Movies/Index'
 import Series from './Components/Series/Index'
-import Trendings from './Components/Trendings/Index'
 import Movies from './Components/Movies/Index'
 
 function App() {
@@ -13,22 +11,19 @@ function App() {
       <div className='headerNavegacao'>
         <BrowserRouter>
           <ul>
-          <li className='menu-item'><Link to="/home">Home</Link></li>
-          <li className='menu-item'><Link to="/movies">Movies</Link></li>
-          <li className='menu-item'><Link to="/series">Series</Link></li>
-          <li className='menu-item'><Link to="/trendings">Trendings</Link></li>
+            <li className='menu-item'><Link to="/home">Home</Link></li>
+            <li className='menu-item'><Link to="/movies">Movies</Link></li>
+            <li className='menu-item'><Link to="/series">Series</Link></li>
           </ul>
 
           <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/movies" element={<Movies/>}/>
-          <Route path="/series" element={<Series/>}/>
-          <Route path="/trendings" element={<Trendings/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
           </Routes>
         </BrowserRouter>
       </div>
-
     </div>
   )
 }
